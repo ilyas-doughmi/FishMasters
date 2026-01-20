a92f2892b81b769a64a7248fdd76848f12a01690
Oussamaycode <oussamalamlouki047@gmail.com>
2026-01-20 14:51:00 +0100

    Overriding login function

---
+++ app/models/AdminModel.php
@@ -0,0 +1,24 @@
+
+<?php
+
+spl_autoload_register($className){
+
+    $path="models";
+    $extension="./php";
+    $fullpath=$path.$className.$extension;
+
+    if(!file_exists($fullpath)){
+        return false;
+    }
+
+    require_once "$fullpath";
+}
+
+class Admin extends UserModel
+
+{
+    public function login($email,$password){
+        $sql="SELECT * FROM users where useremail=" ;
+    }
+
+} 
---
+++ app/models/UserModel.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class User 
+{
+    protected $id;
+    protected $fullname;
+    protected $email;
+    protected $password;
+    protected $role;
+    protected $createdAt;
+
+    public function __construct($id,$fullname,$email,$password,$role,$createdAt){
+        $this->id=$id;
+        $this->fullname=$fullname;
+        $this->email=$email;
+        $this->password=$password;
+        $this->role=$role;
+        $this->createdAt;
+    }
+
+
+    abstract public function login();
+    abstract public function register();
+
+    public function __get($name) {
+        if ($name === "password") {
+            echo "Password cannot be accessed\n";
+            return null;
+        } else {
+            echo "Getting property: $name\n";
+            return $this->$name;
+        }
+    }
+
+    public function __set($name, $value) {
+        if ($name === "password") {
+            echo "Password cannot be modified\n";
+            return false;
+        } else {
+            $this->$name = $value;
+            return true;
+        }
+    }
+    
+}
