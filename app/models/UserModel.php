13acffd61a03555990f8af9be00940e38de0c8e4
Oussamaycode <oussamalamlouki047@gmail.com>
2026-01-20 12:21:54 +0100

    User model:finished

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,20 +1,34 @@
 <?php
 
 abstract class User 
-
 {
+    protected $id;
+    protected $fullname;
+    protected $email;
+    protected $password;
+    protected $role;
+    protected $createdAt;
 
-   protected $id;
-   protected $fullname;
-   protected $email;
-   protected $password;
-   protected $role;
-   protected $createdAt;
+    abstract public function login();
+    abstract public function register();
 
+    public function __get($name) {
+        if ($name === "password") {
+            echo "Password cannot be accessed\n";
+            return null;
+        } else {
+            echo "Getting property: $name\n";
+            return $this->$name;
+        }
+    }
 
-   abstract public function login();
-   abstract public function register()
-
-   
-
+    public function __set($name, $value) {
+        if ($name === "password") {
+            echo "Password cannot be modified\n";
+            return false;
+        } else {
+            $this->$name = $value;
+            return true;
+        }
+    }
 }
