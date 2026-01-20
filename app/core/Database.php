bacde70a3a2fa5422b904c2f16eea17a84cb4731
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-20 15:38:07 +0100

    feat: Change PDO fetch mode from FETCH_ASSOC to FETCH_CLASS in Database class

--- app/core/Database.php
+++ app/core/Database.php
@@ -19,7 +19,7 @@ class Database
 
             self::$instance = new PDO($dsn, $user, $pass, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,
                 PDO::ATTR_EMULATE_PREPARES => false,
             ]);
         }
