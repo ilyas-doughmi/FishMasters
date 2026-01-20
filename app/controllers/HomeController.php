4d1ea1a858bed7b38726311e682073b1795ba1fc
Oussamaycode <oussamalamlouki047@gmail.com>
2026-01-20 11:23:34 +0100

    use case diagramme

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-class HomeController{
+class HomeController{ // localhost/mabagnole/vehicle/index
     public function index()
     {
         require_once('app/views/home/index.php');
---
+++ app/controllers/LoginController.php
@@ -0,0 +1,8 @@
+<?php
+
+class LoginController{ // localhost/mabagnole/vehicle/index
+    public function index()
+    {
+        require_once('app/views/login/index.php');
+    }
+}
