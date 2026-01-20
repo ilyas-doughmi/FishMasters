7b61d7077a3d4e96067a5eed830f19d5e98b8330
Maryam Zraydi <maryam@example.com>
2026-01-20 15:20:41 +0100

    merging files

---
+++ app/models/species.php
@@ -0,0 +1,43 @@
+<?php
+
+class Species
+{
+    private int $id;
+    private string $name;
+    private float $minSize;
+    private float $coefficient;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function getMinSize(): float
+    {
+        return $this->minSize;
+    }
+    public function getCoefficient(): float
+    {
+        return $this->coefficient;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+    public function setMinSize(float $minSize): void
+    {
+        $this->minSize = $minSize;
+    }
+    public function setCoefficient(float $coefficient): void
+    {
+        $this->coefficient = $coefficient;
+    }
+}
