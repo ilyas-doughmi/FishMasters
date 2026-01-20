452a3d3eb97909da3fbb899732480b84a54fdc11
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-01-20 10:17:27 +0100

    feat: Implement basic front controller pattern with URL rewriting and routing to controllers and methods.

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+
+$urlParts = explode('/',rtrim($url,'/'));
+
+$controllerName = ucfirst($urlParts[0]) . 'Controller';
+
+$controllerFile = 'app/controllers/' . $controllerName . '.php';
+
+if(file_exists($controllerFile))
+{
+    require_once($controllerFile);
+    $controller = new $controllerName();
+}
+else{
+    echo "not found";
+}
+
+
+if(isset($urlParts[1]))
+{
+    $method = $urlParts[1];
+}
+else{
+    $method = 'index';
+}
+
+
+if(method_exists($controller,$method)){
+    call_user_func_array([$controller,$method],$urlParts);
+}
+else{
+    echo "methode not found";
+}
