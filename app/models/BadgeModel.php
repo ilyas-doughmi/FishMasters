d3be9d6a7cfc2b73ec6fced1930905cc151d4183
Oussamaycode <oussamalamlouki047@gmail.com>
2026-01-21 11:53:37 +0100

    Badge Model:Finished

---
+++ app/models/BadgeModel.php
@@ -0,0 +1,28 @@
+<?php
+
+class Badge
+{
+    private int $id;
+    private string $name;
+    private string $description;
+
+    public function __construct(
+        int $id,
+        string $name,
+        string $description
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+    public function __set($property, $value)
+    {
+        $this->$property = $value;
+    }
+}
