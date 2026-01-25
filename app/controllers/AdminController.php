0a90554e0f71995151b1126c39537374832cb812
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-01-25 16:27:00 +0100

    feat : add all function needed in admin controller

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -111,19 +111,115 @@ class AdminController
         require_once 'app/views/admin/competitions/edit.php';
     }
 
+    public function competitions_delete()
+    {
+        require_once 'app/core/Database.php';
+        require_once 'app/models/CompetitionModel.php';
+        require_once 'app/models/ScoringRulesModel.php';
+
+        $db = Database::getInstance();
+
+        $competitionModel = new CompetitionModel($db);
+        $ruleModel = new ScoringRulesModel($db);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['idClose'])) {
+            $result = $competitionModel->close($_POST['idClose']);
+            if ($result) {
+                header('Location: index.php?url=admin/competitions');
+                exit;
+            }
+        }
+        require_once 'app/views/admin/competitions';
+    }
 
     public function rules()
     {
+        require_once('app/core/Database.php');
+        require_once('app/models/ScoringRulesModel.php');
+
+        $db = Database::getInstance();
+        $ScoringRulesModel = new ScoringRulesModel($db);
+        $ScoringRules = $ScoringRulesModel->showRules();
+
         require_once('app/views/admin/rules/index.php');
     }
 
     public function rules_add()
     {
-        require_once('app/views/admin/rules/add.php');
+        require_once 'app/views/admin/rules/add.php';
+    }
+
+    public function rules_create()
+    {
+
+        require_once('app/core/Database.php');
+        require_once('app/models/ScoringRulesModel.php');
+
+        $db = Database::getInstance();
+
+        $rule = new ScoringRulesModel($db);
+        $rule->setName($_POST['ruleName']);
+        $rule->setDescription($_POST['ruleDescription']);
+
+        if ($rule->addRule()) {
+            header('Location: /fishmasters/admin/rules');
+            exit;
+        }
+    }
+
+    public function rules_edit()
+    {
+        require_once('app/core/Database.php');
+        require_once('app/models/ScoringRulesModel.php');
+
+        $db = Database::getInstance();
+
+        $rule = new ScoringRulesModel($db);
+        $rule = $rule->getRuleById((int) $_POST['editId']);
+
+        require_once 'app/views/admin/rules/edit.php';
+    }
+
+    public function rules_update()
+    {
+        require_once('app/core/Database.php');
+        require_once('app/models/ScoringRulesModel.php');
+
+        $db = Database::getInstance();
+
+        $rule = new ScoringRulesModel($db);
+        $rule->setName($_POST['ruleName']);
+        $rule->setDescription($_POST['ruleDescription']);
+
+        if ($rule->editRule((int) $_POST['ruleId'])) {
+            header('Location: /fishmasters/admin/rules');
+            exit;
+        }
+    }
+
+    public function rules_delete()
+    {
+        require_once('app/core/Database.php');
+        require_once('app/models/ScoringRulesModel.php');
+
+        $db = Database::getInstance();
+
+        $rule = new ScoringRulesModel($db);
+
+        if ($rule->deleteRule((int) $_POST['ruleId'])) {
+            header('Location: /fishmasters/admin/rules');
+            exit;
+        }
     }
 
     public function species()
     {
+        require_once('app/core/Database.php');
+        require_once('app/models/SpeciesModel.php');
+
+        $db = Database::getInstance();
+        $SpeciesModel = new SpeciesModel($db);
+        $SpeciesModel = $SpeciesModel->getAll();
+
         require_once('app/views/admin/species/index.php');
     }
 
@@ -131,11 +227,87 @@ class AdminController
     {
         require_once('app/views/admin/species/add.php');
     }
+    public function species_edit()
+    {
+        require_once('app/core/Database.php');
+        require_once('app/models/SpeciesModel.php');
+
+        $db = Database::getInstance();
+        $id = (int) $_POST['speciesId'];
+
+        $speciesModel = new SpeciesModel($db);
+        $species = $speciesModel->getById($id);
+
+
+        require_once 'app/views/admin/species/edit.php';
+    }
+
+    public function species_update()
+    {
+
+        require_once('app/core/Database.php');
+        require_once('app/models/SpeciesModel.php');
+
+        $db = Database::getInstance();
+        $id = (int) $_POST['speciesId'];
+
+        $speciesModel = new SpeciesModel($db);
+        $speciesModel->setName($_POST['speciesName']);
+        $speciesModel->setMinSize($_POST['speciesMinSize']);
+        $speciesModel->setCoefficient($_POST['coefficient']);
+
+        $speciesModel->update($id);
+
+        header('Location: /fishmasters/index.php?url=Admin/species');
+        exit;
+    }
+
+    public function species_delete()
+    {
+        require_once('app/core/Database.php');
+        require_once('app/models/SpeciesModel.php');
+
+        $db = Database::getInstance();
+        $id = (int) $_POST['speciesId'];
+
+        $speciesModel = new SpeciesModel($db);
+        $speciesModel->delete($id);
+
+        header('Location: /fishmasters/index.php?url=Admin/species');
+        exit;
+    }
+    public function species_create()
+    {
+        require_once('app/core/Database.php');
+        require_once('app/models/SpeciesModel.php');
+
+        $db = Database::getInstance();
+
+        $speciesModel = new SpeciesModel($db);
+        $speciesModel->setName($_POST['speciesName']);
+        $speciesModel->setMinSize($_POST['speciesMinSize']);
+        $speciesModel->setCoefficient($_POST['coefficient']);
+
+        $speciesModel->create();
+
+        header('Location: /fishmasters/index.php?url=Admin/species');
+        exit;
+    }
+
+
 
     public function catches_pending()
     {
+        require_once('app/core/Database.php');
+        require_once('app/models/CatchModel.php');
+
+        $db = Database::getInstance();
+        $CatchModel = new CatchFish($db);
+        $catches = $CatchModel->getPending();
+
         require_once('app/views/admin/catches/pending.php');
     }
+
 
     public function catches_review()
     {
@@ -149,11 +321,24 @@ class AdminController
 
     public function fishers()
     {
+        require_once('app/core/Database.php');
+        require_once('app/models/FisherModel.php');
+
+        $db = Database::getInstance();
+        $FisherModel = new Fisher($db);
+        $allFishers = $FisherModel->show();
         require_once('app/views/admin/fishers/index.php');
     }
 
-    public function fishers_show()
+    // Show single fisher details
+    public function fishers_show($id)
     {
+        require_once('app/core/Database.php');
+        require_once('app/models/FisherModel.php');
+
+        $db = Database::getInstance();
+        $FisherModel = new Fisher($db);
+        $fisher = $FisherModel->getById($id);
         require_once('app/views/admin/fishers/show.php');
     }
 
